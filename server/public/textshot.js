966c7d5c153f6f31faf51acb3d3e1ae90d7f42a1
Ryan Catalani <catalani.ryan@gmail.com>
2015-05-21 14:55:32 -0400

    Add tweet composer, initial styles.

--- server/public/textshot.js
+++ server/public/textshot.js
@@ -15,6 +15,9 @@ window.onload = function() {
 	var quote_title_el = document.getElementById('quote_title');
 	var quote_source_el = document.getElementById('quote_source');
 
+	var tweet_composer_el = document.getElementById('tweet_input_composer');
+	var tweet_count_el = document.getElementById('tweet_input_count');
+
 	style = {
 		bkgd: '#FCFAEC',
 		sec_bkgd: '#EEECD8',
@@ -29,14 +32,17 @@ window.onload = function() {
 	quote_text_el.onkeyup = function() { update_quote(); }
 	quote_title_el.onkeyup = function() { update_quote(); }
 	quote_source_el.onkeyup = function() { update_quote(); }
+	tweet_composer_el.onkeyup = function() { update_tweet_char_count(); }
 
 	var test_text = function() {
 		quote_text_el.value = "With a few taps on a phone, for a fee, today’s hottest start-ups will help people on the lowest rungs of the 1 percent live like their betters in the 0.1 percent. These services give the modestly wealthy a chance to enjoy the cooks, cleaners, drivers, personal assistants and all the other lavish appointments that have defined extravagant wealth. As one critic tweeted, San Francisco’s tech industry “is focused on solving one problem: What is my mother no longer doing for me?”";
 		// quote_text_el.value = "No, no, say the start-ups that, today, look as if they’re targeting the rich. The nature of the tech business is that costs come down.";
 		quote_title_el.value = "A Tech Boom Aimed at the Few Instead of the World";
 		quote_source_el.value = "The New York Times";
+		tweet_composer_el.value = "http://www.nytimes.com/2015/05/21/technology/personaltech/a-tech-boom-aimed-at-the-few-instead-of-the-world.html?ref=technology";
+		
 		update_quote();
-
+		update_tweet_char_count();
 	}
 
 	var update_quote = function() {
@@ -47,12 +53,32 @@ window.onload = function() {
 		// draw_bounds();
 	}
 
+	var update_tweet_char_count = function() {
+		var count = 140;
+		var tweet = tweet_composer_el.value;
+		var words = tweet.split(' ');
+		var link_len = 23;
+
+		count -= link_len; // 1 link for the image
+		count -= words.length; // Spaces
+
+		for (var i = words.length - 1; i >= 0; i--) {
+			var word = words[i];
+			if (word.search(/https?:\/\/.+[.].+/) != -1) { // Liberal
+				count -= link_len;
+			} else {
+				count -= word.length;	
+			}
+		};
+
+		tweet_count_el.innerText = count;
+	}
+
 	var setup_canvas = function() {
 		canvas = document.getElementById('textshot');
 		canvas.width = quote_width * t_ratio;
 		canvas.height = quote_height * t_ratio;
-		canvas.style.width = quote_width + "px";
-		canvas.style.height = quote_height + "px";
+		size_canvas(canvas.parentElement.offsetWidth, canvas.parentElement.offsetWidth / 2)
 
 		ctx = canvas.getContext('2d');
 		ctx.setTransform(t_ratio, 0, 0, t_ratio, 0, 0);
@@ -77,6 +103,13 @@ window.onload = function() {
 		};
 
 		clear_canvas();
+	}
+
+	var size_canvas = function(w, h) {
+		var w = defaultFor(w, quote_width);
+		var h = defaultFor(h, quote_height);
+		canvas.style.width = w + "px";
+		canvas.style.height = h + "px";		
 	}
 
 	var clear_canvas = function() {
